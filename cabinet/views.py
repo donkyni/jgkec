850e26e94773d44865843b9c20400cdb5bc9e59a
donkyni <49393917+donkyni@users.noreply.github.com>
2021-01-05 12:01:28 +0000

    vInitial commit

---
+++ cabinet/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+
+
+def acceuil(request):
+    return render(request, 'acceuil.html', locals())
+
+
+def contacts(request):
+    return render(request, 'contacts.html', locals())
+
+
+def expertise(request):
+    return render(request, 'expertise/expertise.html', locals())
+
+
+def ingenierie(request):
+    return render(request, 'expertise/ingenierie.html', locals())
+
+
+def management(request):
+    return render(request, 'expertise/management.html', locals())
+
+
+def finance(request):
+    return render(request, 'expertise/financement.html', locals())
+
+
+def developpement(request):
+    return render(request, 'expertise/developpement.html', locals())
+
+
+def insertion(request):
+    return render(request, 'expertise/insertion.html', locals())
+
+
+def formation(request):
+    return render(request, 'formation/formations.html', locals())
+
+
+def formule(request):
+    return render(request, 'formation/formule.html', locals())
+
+
+def calendrier(request):
+    return render(request, 'formation/calendrier.html', locals())
---
+++ jgk/urls.py
@@ -0,0 +1,43 @@
+"""jgk URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from cabinet.views import acceuil, contacts, expertise, ingenierie, management, finance, developpement, insertion, \
+    formation, formule, calendrier
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', acceuil, name="acceuil"),
+    path('contacts/', contacts, name="contacts"),
+    path('expertise/', expertise, name="expertise"),
+    path('ingenierie/', ingenierie, name="ingenierie"),
+    path('management/', management, name="management"),
+    path('finance/', finance, name="finance"),
+    path('developpement', developpement, name="developpement"),
+    path('insertion/', insertion, name="insertion"),
+    path('formation/', formation, name="formation"),
+    path('formule/', formule, name="formule"),
+    path('calendrier/', calendrier, name="calendrier"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
