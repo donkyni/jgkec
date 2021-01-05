af9bb89a1b1c47192d8e7aae7b22f66382f1360b
donkyni <49393917+donkyni@users.noreply.github.com>
2021-01-05 15:50:54 +0000

    Modified seetings.py / ALLOWED_HOSTS

--- jgk/settings.py
+++ jgk/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,9 +25,8 @@ SECRET_KEY = '&1epeqanmx5aunr%wlaurxnqrn*7h_uw%4s)+z7@co)dh_=f@b'
 DEBUG = False
 
 ALLOWED_HOSTS = [
-	'*'
+    '*'
 ]
-
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jgk.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -133,5 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/fichier_static/'
 
 MEDIA_ROOT = (
-    BASE_DIR / 'fichier_static'
+        BASE_DIR / 'fichier_static'
 )
