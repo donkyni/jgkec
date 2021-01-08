bdba09741a1bece8cb2892561b77df6c6b3f8f13
donkyni <49393917+donkyni@users.noreply.github.com>
2021-01-08 18:32:37 +0000

    update wsgi.py

--- jgk/wsgi.py
+++ jgk/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+sys.path.append("/var/www/html/django/jgk/")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jgk.settings')
+
+application = get_wsgi_application()
+
+
+"""
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jgk.settings')
 
 application = get_wsgi_application()
+"""
