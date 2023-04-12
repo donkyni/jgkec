2416b0ce18915cd4ddd8ef31eaa317dfed43da23
SOGNIKIN Yao Donald <ysognikin@moov-africa.tg>
2023-04-12 16:28:05 +0000

    add models Accueil

--- cabinet/models.py
+++ cabinet/models.py
@@ -28,3 +28,19 @@ class Activite(models.Model):
     texte = models.TextField(verbose_name="Texte", null=True, blank=False)
     date = models.DateTimeField(null=True, auto_now_add=True, blank=True)
     archive = models.BooleanField(default=False)
+
+
+"""
+Cette section concerne la modification 
+des differentes sections sur les pages
+
+
+
+class Accueil(models.Model):
+    banniere_accueil = models.TextField(verbose_name="Bannière déroulante", null=True, blank=False)
+    notre_mission = models.TextField(verbose_name="Bannière notre mission", null=True, blank=False)
+    image_productivite = models.ImageField(verbose_name="Image boostez votre productivité", blank=True, null=True, upload_to="accueil")
+    texte_productivite = models.TextField(verbose_name="Texte boostez votre productivité", null=True, blank=False)
+    nos_solutions = models.TextField(verbose_name="Texte nos solutions", null=True, blank=False)
+
+"""
